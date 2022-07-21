c9c4e6e139a3b3b30b5f8f2b2db1698e13c2a384
LegibleMedal670 <legiblemedal670@gmail.com>
2022-07-21 22:01:43 +0900

    firebase floating button

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -15,37 +17,81 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-
   final PageController pageViewController = PageController();
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: PageView(
-          controller: pageViewController,
-          scrollDirection: Axis.vertical,
-          children: [
-            Container(
-              color: Colors.black,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('알고 계셨나요?',style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0
-                    ),),
-                    Text('이곳에 내용을 받아서 넣어줄것 \nddd', style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0
-                    ),)
-                  ],
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('tmi/I0GzJYmu5PWG6zxUBjgB/information')
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            final infos = snapshot.data!.docs;
+            return PageView.builder(
+              controller: pageViewController,
+              scrollDirection: Axis.vertical,
+              itemCount: infos.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.black,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '알고 계셨나요?',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 15.0, right: 15.0),
+                          child: Text(
+                            infos[index]['tmi'],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        floatingActionButton: Container(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.star
+                  ),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Icon(
+                      Icons.search
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
